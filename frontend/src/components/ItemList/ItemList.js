1f8314654a37c2cbce21f96eb698c134105f78e2
puyomi <rex0912@naver.com>
2018-11-11 17:24:05 +0900

    feed item 수정완료

--- frontend/src/components/ItemList/ItemList.js
+++ frontend/src/components/ItemList/ItemList.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const ItemList = props => {
-  return <div>{props.feed}</div>;
+const ItemList = ({ creator, subject, content }) => {
+  return (
+    <li>
+      작성자{creator}, 제목{subject}, 내용{content}
+    </li>
+  );
 };
 
 export default ItemList;
--- frontend/src/containers/ItemListContainer.js
+++ frontend/src/containers/ItemListContainer.js
@@ -4,6 +4,10 @@ import { actionCreators as itemActions } from "store/modules/item";
 import { ItemList } from "components";
 
 class ItemListContainer extends Component {
+  state = {
+    feedList: []
+  };
+
   componentDidMount() {
     const { feedItem } = this.props;
     feedItem();
@@ -11,14 +15,30 @@ class ItemListContainer extends Component {
 
   render() {
     const { feed } = this.props;
-    return <ItemList feed={feed} />;
+    console.log(feed);
+    return (
+      <div>
+        <ul>
+          {feed.map(item => {
+            return (
+              <ItemList
+                creator={item.creator.username}
+                subject={item.subject}
+                content={item.content}
+                key={item.id}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
-  const { feed } = state;
+  const { feed } = state.item;
   return {
-    feed: feed
+    feed
   };
 };
 
